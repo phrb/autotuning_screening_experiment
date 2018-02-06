ae1a9386b2de25cbe7b3b2920362a5d863d057b4
phrb <pedro.bruel@gmail.com>
2018-02-06 11:05:25 +0100

    Add functions to generate commands, compile and run

--- src/screening/screening.jl
+++ src/screening/screening.jl
@@ -30,7 +30,7 @@ function generate_search_space(filename::String)
                     end
                 elseif parameter_type == "enumeration_parameters"
                     for parameter in json_data[key][parameter_type]
-                        push!(parameters, Factor(Enumeration, 
+                        push!(parameters, Factor(Enumeration,
                                                  "$prefix $(parameter[1])",
                                                  "$(parameter[2][1])",
                                                  "$(parameter[2][end])"))
@@ -50,15 +50,98 @@ function generate_search_space(filename::String)
 end
 
 function generate_design(factors::Array{Factor, 1})
-    plackett_burman(length(factors) + 2)
+    # TODO: Fix this. Plackett-Burman should return designs larger
+    #       than requested rather than smaller.
+    plackett_burman(length(factors) + 4)
 end
 
 function generate_experiments(design::Array{Int, 2}, factors::Array{Factor, 1})
+    design_size = size(design)
+    factors_length = length(factors)
+
+    measurements = Array{Measurement, 1}()
+
+    for i = 1:design_size[2]
+        levels = Dict{Symbol, Any}()
+
+        for j = 1:factors_length
+            if design[i, j] == 1
+                levels[Symbol(factors[j].name)] = factors[j].max
+            else
+                levels[Symbol(factors[j].name)] = factors[j].min
+            end
+        end
+
+        push!(measurements, Measurement(levels))
+    end
+
+    init_table(measurements)
+end
+
+function generate_commands(experiments::NextTable)
+    commands = Dict{UInt, String}()
+
+    for row in experiments
+        command = string()
+
+        for (flag, value) in zip(keys(row), row)
+            if flag != :response && flag != :complete && flag != :id
+                if value == "on"
+                    command = string(command, flag, " ")
+                elseif value != "off"
+                    try
+                        parse(Int, value)
+                    catch ArgumentError
+                        value = string("\"", value, "\"")
+                    end
+
+                    command = string(command, flag, value, " ")
+                end
+            end
+        end
+
+        commands[row.id] = strip(command)
+    end
+
+    commands
+end
+
+function log_state()
+end
+
+function compile(command::String)
+    environment = copy(ENV)
+    environment["NVCC_FLAGS"] = command
+
+    directory = "../gaussian/"
+
+    c = Cmd(`make`, env = environment, dir = directory)
+
+    run(c)
+end
+
+function measure()
+    directory = "../gaussian/"
+    c = Cmd(`./run.sh`, dir = directory)
+
+    run(c)
 end
 
 function run_experiments()
     factors = generate_search_space("../parameters/nvcc_flags.json")
     design = generate_design(factors)
+    experiments = generate_experiments(design, factors)
+
+    save_to_disk(experiments, "./experiments.csv")
+
+    commands = generate_commands(experiments)
+
+    for (id, command) in commands
+        println(id)
+        log_state()
+        compile(command)
+        measure()
+    end
 end
 
 run_experiments()
